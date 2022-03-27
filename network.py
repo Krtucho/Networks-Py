5a1df1c25f1f05a38a84dd877daf6ad2a2acd7a0
lauolivia <91337957+lauolivia@users.noreply.github.com>
2022-03-27 14:29:09 -0400

    creando branch

--- network.py
+++ network.py
@@ -1,7 +1,10 @@
 from Host import Host
 from Hub import Hub
-
+#from Methods import Methods
 signal_time: int = 10
+
+def parser(list):
+    
 
 
 def create_device(type="host", name="", n_ports=1):
@@ -16,17 +19,22 @@ def start():
     items = open("script.txt", 'r')
     lists = items.read().split("\n")
     lists = [item.split() for item in lists]
-    print(lists)
     
+    #instructions=parser(lists)
     time: int = 0
     finished: bool = len(lists) == 0
     index = 0
     
-    devices: list = []
+    devices: list = []    
     
     while not finished:
         actual = lists[index]
-        while actual[0] == 
-            actual = lists[index]
+        while (int)actual[0] == time
+            #actual = lists[index]
+            
+
+
+
+        time++
     
 start()
